107c22f3ea85bb96268474e14cb08fa6f57a0ef8
Tayfun Kahveci <tkahveci87@hotmail.com>
2025-01-11 10:41:09 +0300

    Fix dependencies

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"q1HEU0b019Tl8Wa8TUjSGIYn4DDeEuEhWIk4NDz66vs=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"LQ6YkovDTxrUBVIujPg0t9y3dgLCukb0sjC5bsNDxmY=\"}"
